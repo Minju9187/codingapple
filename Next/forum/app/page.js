7f50deb9d169b4c579183595721c048123cdf5c8
minju <alswn1583@naver.com>
2024-03-12 12:01:52 +0900

    [2024/03/12] Next 공부

--- Next/forum/app/page.js
+++ Next/forum/app/page.js
@@ -6,13 +6,13 @@ import styles from "./page.module.css";
 
 //DB 입출력 코드는 server component 안에서만 쓰자!!
 export default async function Home() {
-  //결과 저장해두고 가져다 씀
-  await fetch("/URL", { cache: "force-cache" });
-  await fetch("/URL");
-  //실시간 데이터가 중요해 캐시를 쓰지 않을때
-  await fetch("/URL", { cache: "no-store" });
-  //60초마다 캐싱된 데이터 갱신
-  await fetch("/URL", { next: { revalidate: 60 } });
+  // //결과 저장해두고 가져다 씀
+  // await fetch("/URL", { cache: "force-cache" });
+  // await fetch("/URL");
+  // //실시간 데이터가 중요해 캐시를 쓰지 않을때
+  // await fetch("/URL", { cache: "no-store" });
+  // //60초마다 캐싱된 데이터 갱신
+  // await fetch("/URL", { next: { revalidate: 60 } });
 
   return <div>안녕</div>;
 }
