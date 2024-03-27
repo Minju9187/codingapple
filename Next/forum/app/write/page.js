ddf2e6945887561075a7e8911a331b44a23ed00e
minju <alswn1583@naver.com>
2024-03-27 14:47:42 +0900

    [2024/03/27] Next 공부

--- Next/forum/app/write/page.js
+++ Next/forum/app/write/page.js
@@ -1,12 +1,43 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 export default function Write() {
+  let [src, setSrc] = useState("");
+
   return (
     <div className="p-20">
       <h4>글작성</h4>
       <form action="api/post/new" method="POST">
         <input name="title" placeholder="글제목" />
         <input name="content" placeholder="글내용" />
+        <input
+          type="file"
+          accept="image/*"
+          onChange={async (e) => {
+            let file = e.target.files[0];
+            let filename = encodeURIComponent(file.name);
+            let res = await fetch("/api/post/image?file=" + filename);
+            res = await res.json();
+
+            //S3 업로드
+            const formData = new FormData();
+            Object.entries({ ...res.fields, file }).forEach(([key, value]) => {
+              formData.append(key, value);
+            });
+            let 업로드결과 = await fetch(res.url, {
+              method: "POST",
+              body: formData,
+            });
+            setSrc(업로드결과.url + "/" + filename);
+
+            if (업로드결과.ok) {
+              setSrc(업로드결과.url + "/" + filename);
+            } else {
+              console.log("실패");
+            }
+          }}
+        />
+        <img src={src} />
         <button type="submit">버튼</button>
       </form>
     </div>
